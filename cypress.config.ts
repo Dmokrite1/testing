ae37dfea053c61b91c0e4bf1a627f51e3f376189
Dmokrite1 <hg_greg@proton.me>
2024-03-04 11:04:09 +0100

    added cypress with ng e2e, use cypress + lighthouse
    Signed-off-by: Dmokrite1 <hg_greg@proton.me>

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,10 +1,20 @@
-import { defineConfig } from 'cypress'
+import { defineConfig } from 'cypress';
+import { lighthouse, prepareAudit } from "@cypress-audit/lighthouse";
 
 export default defineConfig({
-  
+
   e2e: {
-    'baseUrl': 'http://localhost:4200'
+    'baseUrl': 'http://localhost:4200',
+    experimentalStudio: true,
+    taskTimeout: 120 * 1000,
+    setupNodeEvents(on, config) {
+      on("before:browser:launch", (_, launchOptions) => {
+        prepareAudit(launchOptions);
+      });
+
+      on("task", {
+        lighthouse: lighthouse()
+      });
+    },
   },
-  
-  
 })
--- cypress/e2e/spec.cy.ts
+++ cypress/e2e/spec.cy.ts
@@ -1,6 +1,38 @@
 describe('My First Test', () => {
   it('Visits the initial project page', () => {
-    cy.visit('/')
-    cy.contains('Tour of Heroes')
-  })
+    cy.visit('/');
+    cy.contains('Tour of Heroes');
+  });
+
+  it('when i click on heroes button, it should show all heroes', () => {
+    cy.visit('/');
+    cy.get('[routerlink="/heroes"').click();
+    cy.get('.heroes > :nth-child(1)').should('exist');
+
+    cy.get('[routerlink="/heroes"]').click();
+    cy.get(':nth-child(1) > a').click();
+    cy.get('label').click();
+    cy.get('#hero-name').click();
+    cy.contains('DR. NICE')
+  });
+
+  it('Should add a hero to the list when clicking on ass an hero button', () => {
+    cy.visit('http://localhost:4200/');
+    cy.get('[routerlink="/heroes"]').click();
+    cy.get('.add-button').click();
+    cy.get('#new-hero').type('TRUC');
+    cy.get('.add-button').click();
+  });
+
+  it('its passes lighthouse test on homepage', () => {
+    cy.visit('/');
+    cy.lighthouse({
+      performance: 80,
+    });
+  });
 })
+
+
+
+
+
